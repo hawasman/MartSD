4fe16619a335ae1284535789ccd4894f74527eee
HawasMan <hawasman@gmail.com>
2018-01-14 20:06:22 +1400

    add shoutem ui

--- App.js
+++ App.js
@@ -11,6 +11,8 @@ import {
   Text,
   View
 } from 'react-native';
+import { NavigationBar, ListView, Screen, Image, Title, Subtitle, Divider, TouchableOpacity, Card  } from '@shoutem/ui';
+
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -20,34 +22,99 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component<{}> {
-  render() {
+  constructor(props) {
+    super(props);
+    this.renderRow = this.renderRow.bind(this);
+    this.state = {
+      restaurants: [{
+        "name": "Gaspar Brasserie",
+        "address": "185 Sutter St, San Francisco, CA 94109",
+        "image": { "url": "https://shoutem.github.io/static/getting-started/restaurant-1.jpg" },
+      }, {
+        "name": "Chalk Point Kitchen",
+        "address": "527 Broome St, New York, NY 10013",
+        "image": { "url": "https://shoutem.github.io/static/getting-started/restaurant-2.jpg" },
+      }, {
+        "name": "Gaspar Brasserie",
+        "address": "185 Sutter St, San Francisco, CA 94109",
+        "image": { "url": "https://shoutem.github.io/static/getting-started/restaurant-3.jpg" },
+      }],
+    }
+  }
+  renderRow(rowData, sectionId, index) {
+    // rowData contains grouped data for one row,
+    // so we need to remap it into cells and pass to GridRow
+
+    if (index === '0') {
+      return (
+        <TouchableOpacity key={index}>
+          <Image
+            styleName="large"
+            source={{ uri: rowData[0].image.url }}
+          >
+            <Tile>
+              <Title styleName="md-gutter-bottom">{rowData[0].name}</Title>
+              <Subtitle styleName="sm-gutter-horizontal">{rowData[0].address}</Subtitle>
+            </Tile>
+          </Image>
+          <Divider styleName="line" />
+        </TouchableOpacity>
+      );
+    }
+
+    const cellViews = rowData.map((restaurant, id) => {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Hi there,
-          if you're seening this then thankyou for been a tester for this app,
-          if you don't want to then Suck it you are :).
-        </Text>
-      </View>
+        <TouchableOpacity key={id} styleName="flexible">
+          <Card styleName="flexible">
+            <Image
+              styleName="medium-wide"
+              source={{ uri: restaurant.image.url  }}
+            />
+            <View styleName="content">
+              <Subtitle numberOfLines={3}>{restaurant.name}</Subtitle>
+              <View styleName="horizontal">
+                <Caption styleName="collapsible" numberOfLines={2}>{restaurant.address}</Caption>
+              </View>
+            </View>
+          </Card>
+        </TouchableOpacity>
+      );
+    });
+    return (
+      <GridRow columns={2}>
+        {cellViews}
+      </GridRow>
+    );
+  }
+  render() {
+      // Group the restaurants into rows with 2 columns, except for the
+    // first article. The first article is treated as a featured article
+    let isFirstArticle = true;
+    const groupedData = GridRow.groupByRows(this.state.restaurants, 2, () => {
+      if (isFirstArticle) {
+        isFirstArticle = false;
+        return 2;
+      }
+
+      return 1;
+    });
+
+    return (
+      <Screen style={styles.container}>
+        <NavigationBar
+          leftComponent={<Icon name="sidebar" />}
+          centerComponent={<Title>MartSD</Title>}
+          rightComponent={(
+            <Button>
+              <Icon name="cart" />
+            </Button>
+          )}
+        />
+        <ListView
+          data={groupedData}
+          renderRow={this.renderRow}
+        />
+      </Screen>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
--- android/app/src/main/java/com/martsd/MainApplication.java
+++ android/app/src/main/java/com/martsd/MainApplication.java
@@ -3,6 +3,9 @@ package com.martsd;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnative.photoview.PhotoViewPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
 import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
 import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
@@ -26,6 +29,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new PhotoViewPackage(),
+            new LinearGradientPackage(),
+            new VectorIconsPackage(),
             new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appcenterCrashes_whenToSendCrashes)),
             new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appcenterAnalytics_whenToEnableAnalytics)),
             new AppCenterReactNativePackage(MainApplication.this)
