17a247fb6e684dcbe2a7002693b3458eeff88a5a
HawasMan <hawasman@gmail.com>
2018-01-14 22:55:45 +1400

    fixing stuff

--- App.js
+++ App.js
@@ -1,19 +1,6 @@
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  View, 
-  TouchableOpacity
-} from 'react-native';
-import { Text, Button, NavigationBar, ListView, Screen, Image, Title, Subtitle, Divider, Card, GridRow  } from '@shoutem/ui';
+import React, { Component } from 'react';;
+import { Text, View, TouchableOpacity, Button, NavigationBar, ListView, Screen, Image, Title, Subtitle, Divider, Card, GridRow  } from '@shoutem/ui';
 
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 export default class App extends Component<{}> {
   constructor(props) {
