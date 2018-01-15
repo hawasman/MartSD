2ce77dfb1cf2b9adac621b817f3d36267fed3805
HawasMan <hawasman@gmail.com>
2018-01-15 21:00:44 +1400

    Revert "added appcenter"
    This reverts commit e8b15821f51b833531d6d0246933cdd44340188b.

--- android/app/src/main/java/com/martsd/MainApplication.java
+++ android/app/src/main/java/com/martsd/MainApplication.java
@@ -3,12 +3,15 @@ package com.martsd;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+<<<<<<< HEAD
 import com.reactnative.photoview.PhotoViewPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
 import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
 import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
+=======
+>>>>>>> parent of e8b1582... added appcenter
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -28,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
+<<<<<<< HEAD
           new MainReactPackage(),
             new PhotoViewPackage(),
             new LinearGradientPackage(),
@@ -35,6 +39,9 @@ public class MainApplication extends Application implements ReactApplication {
             new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appcenterCrashes_whenToSendCrashes)),
             new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appcenterAnalytics_whenToEnableAnalytics)),
             new AppCenterReactNativePackage(MainApplication.this)
+=======
+          new MainReactPackage()
+>>>>>>> parent of e8b1582... added appcenter
       );
     }
 
